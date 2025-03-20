cd88411bc609598a285f66eb89e8aa3f06cc94b3
ryanc3258 <ryanc3258@outlook.com>
2025-03-19 23:47:34 -0400

    Added Tests and Updated README

--- README.md
+++ README.md
@@ -21,3 +21,15 @@ To run the program: [DevDeck](https://www.cs.uml.edu/~wfondoul/)
 * Frontend: HTML, CSS
 * Backend: -
 * Deployment: UML Server (frontend & backend)
+
+## Cloning Our Project
+
+To clone DevDeck, simply use the command git clone https://github.com/smwfondu/DevDeck.git. Currently, DevDeck is being hosted on 
+https://www.cs.uml.edu/~wfondoul/ and being updated manually from pushes to the GitHub repository. However, work is being done 
+on using coninuous integration tools to automatically push changes to the website.
+
+## Testing
+
+Our testing file for DevDeck is found at unit_tests.cpp and contain all of the unit tests to test the functionality of the 
+GitHub integration (reading data from a GitHub user's repositories). To run the tests simply build them using:
+g++ -o test unit_tests.cpp -lboost_unit_test_framework and run the tests by simply running: ./test
--- unit_tests.cpp
+++ unit_tests.cpp
@@ -1,12 +1,44 @@
 #define BOOST_TEST_MODULE MyTestModule
 #include <boost/test/included/unit_test.hpp>
+#include <sstream>
+#include "github_integration.cpp"
 
 //test case 1
+BOOST_AUTO_TEST_CASE(testFetchGitHubDataStringLength) {
+    string result;
+
+    result = fetchGitHubData("smwfondu");
+    BOOST_REQUIRE_GT(result.length(), 0);
+}
 
 //test case 2
+BOOST_AUTO_TEST_CASE(testGetRepoNamesStringLength) {
+    vector<string> result;
+
+    result = getRepoNames("smwfondu");
+    BOOST_REQUIRE_EQUAL(result.empty(), false);
+}
 
 //test case 3
+BOOST_AUTO_TEST_CASE(testGetRepoLanguagesStringLength) {
+    vector<string> result;
+
+    result = getRepoLanguages("smwfondu");
+    BOOST_REQUIRE_EQUAL(result.empty(), false);
+}
 
 //test case 4
+BOOST_AUTO_TEST_CASE(testGetRepoDescriptionsStringLength) {
+    vector<string> result;
+
+    result = getRepoDescriptions("smwfondu");
+    BOOST_REQUIRE_EQUAL(result.empty(), false);
+}
 
 //test case 5
+BOOST_AUTO_TEST_CASE(testGetRepoURLsStringLength) {
+    vector<string> result;
+
+    result = getRepoURLs("smwfondu");
+    BOOST_REQUIRE_EQUAL(result.empty(), false);
+}
