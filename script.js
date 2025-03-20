98b635f78f2ff32bd3679c09ae4411af264bab03
Vraj Patel <mycsstuff@outlook.com>
2025-03-19 20:28:05 -0400

    Add code that copies the content of the template and puts in the middle column.

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let templateEls = document.querySelectorAll(".template"); // templates
 
+var templateSelectedEl = document.querySelector(".template-selected");
+
 /* Highlight template based on the user selection */
 for (let i = 0; i < templateEls.length; i++) {
     templateEls[i].onclick = function() {
-      // Remove the border from all divs first
-      for (let j = 0; j < templateEls.length; j++) {
+      for (let j = 0; j < templateEls.length; j++) { // Remove the border from all divs first
         templateEls[j].style.border = "solid 4px transparent";
       }
-      // Apply the border to current element
-      this.style.border = "solid 4px red";
+      this.style.border = "solid 4px red"; // Apply the border to current element
+      templateSelectedEl.innerHTML = this.innerHTML; // Display this element on the middle column
     };
 }
 
@@ -17,11 +18,9 @@ let projectEls = document.querySelectorAll(".project"); // templates
 /* Highlight GitHub project based on the user selection */
 for (let i = 0; i < projectEls.length; i++) {
   projectEls[i].onclick = function() {
-      // Remove the border from all divs first
-      for (let j = 0; j < projectEls.length; j++) {
+      for (let j = 0; j < projectEls.length; j++) { // Remove the border from all divs first
         projectEls[j].style.border = "solid 4px transparent";
       }
-      // Apply the border to current element
-      this.style.border = "solid 4px red";
+      this.style.border = "solid 4px red"; // Apply the border to current element
     };
 }
