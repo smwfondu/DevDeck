a964ca5480aba9162d1a0ef0195e52e8e64c1950
Vraj Patel <mycsstuff@outlook.com>
2025-03-19 20:12:38 -0400

    Add similar code - as template selection - for github project to highlight based on the user selection.

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ for (let i = 0; i < templateEls.length; i++) {
       this.style.border = "solid 4px red";
     };
 }
+
+let projectEls = document.querySelectorAll(".project"); // templates
+
+/* Highlight GitHub project based on the user selection */
+for (let i = 0; i < projectEls.length; i++) {
+  projectEls[i].onclick = function() {
+      // Remove the border from all divs first
+      for (let j = 0; j < projectEls.length; j++) {
+        projectEls[j].style.border = "solid 4px transparent";
+      }
+      // Apply the border to current element
+      this.style.border = "solid 4px red";
+    };
+}
