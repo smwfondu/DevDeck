961e6b6ba7030cac9ebd612e0e2634d481542485
Vraj Patel <mycsstuff@outlook.com>
2025-03-20 21:18:29 -0400

    Add templates.json. Add an array of template objects. Each template object has 3 styles with other properties.
    Add the template selection code into a addEventListener function. Add the data fetching (form the templates.json file) code to the function.

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-let templateEls = document.querySelectorAll(".template"); // templates
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('templates.json') // fetch the JSON data
+    .then(function(response) {
+      return response.json(); // convert into javascript objects
+    })
+      .then(function(templates) {
+        let templateEls = document.querySelectorAll(".template"); // templates
 
-var templateSelectedEl = document.querySelector(".template-selected");
+        var templateSelectedEl = document.querySelector(".template-selected");
 
-/* Highlight template based on the user selection */
-for (let i = 0; i < templateEls.length; i++) {
-    templateEls[i].onclick = function() {
-      for (let j = 0; j < templateEls.length; j++) { // Remove the border from all divs first
-        templateEls[j].style.border = "solid 4px transparent";
-      }
-      this.style.border = "solid 4px red"; // Apply the border to current element
-      templateSelectedEl.innerHTML = this.innerHTML; // Display this element on the middle column
-    };
-}
+        /* Highlight template based on the user selection */
+        for (let i = 0; i < templateEls.length; i++) {
+            templateEls[i].onclick = function() {
+              for (let j = 0; j < templateEls.length; j++) { // Remove the border from all divs first
+                templateEls[j].style.border = "solid 4px transparent";
+              }
+              this.style.border = "solid 4px red"; // Apply the border to current element
+              templateSelectedEl.innerHTML = this.innerHTML; // Display this element on the middle column
+              
+              // apply the styles from the JSON
+              let selectedTemplate = templates[i];
+              let styles = selectedTemplate.styles;
+
+              templateSelectedEl.style.backgroundColor = styles.backgroundColor;
+              templateSelectedEl.style.color = styles.color;
+              templateSelectedEl.style.fontFamily = styles.fontFamily;
+            };
+        }
+    })
+    .catch(function(error){
+      console.error('Error loading templates:', error);
+    });
+});
+
 
 let projectEls = document.querySelectorAll(".project"); // templates
 
